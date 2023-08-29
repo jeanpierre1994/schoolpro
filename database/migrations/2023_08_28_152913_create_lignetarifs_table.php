19f3f730346675be3235a1e94edd33c73e482332
jeanpierre1994 <jeansneiba.7@gmail.com>
2023-08-29 14:18:44 +0100

    update tb paiement scolarite

--- database/migrations/2023_08_28_152913_create_lignetarifs_table.php
+++ database/migrations/2023_08_28_152913_create_lignetarifs_table.php
@@ -14,7 +14,21 @@ return new class extends Migration
     public function up()
     {
         Schema::create('lignetarifs', function (Blueprint $table) {
-            $table->id();
+            $table->unsignedBigInteger("id")->autoIncrement(); 
+            $table->string('code',100)->unique()->nullable();  
+            $table->boolean('is_required')->default(true);
+            $table->unsignedBigInteger('gp_id'); 
+            $table->unsignedBigInteger('rubrique_id'); 
+            $table->integer('ligne')->default(0);   
+            $table->integer('montant')->default(0);   
+            $table->unsignedBigInteger('statut_id');   
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by')->nullable();  
+            $table->foreign('gp_id')->references('id')->on('groupepedagogiques');
+            $table->foreign('rubrique_id')->references('id')->on('rubriques'); 
+            $table->foreign('statut_id')->references('id')->on('statuts'); 
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('updated_by')->references('id')->on('users'); 
             $table->timestamps();
         });
     }
---
+++ database/migrations/2023_08_29_125228_add_new_column_grille.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('groupepedagogiques', function (Blueprint $table) {  
+            $table->unsignedBigInteger('grilletarifaire_id');   
+            $table->foreign('grilletarifaire_id')->references('id')->on('grilletarifaires');   
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
