ec3c0b4e36e2e676cf8855daf048cf096f7a132f
Tamuzo77 <tamuzomenga@gmail.com>
2024-03-03 14:26:41 +0100

    modification des utilisateurs

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles, FilterByProf;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
